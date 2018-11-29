3ddd1211cb4e4597f895b0aa3a8080842a17a60e
josh <josh@tesseractmobile.com>
2018-11-29 00:33:58 -0600

    Added UDP controll

--- sketch_nov23a_robot_web_controlled/sketch_nov23a_robot_web_controlled.ino
+++ sketch_nov23a_robot_web_controlled/sketch_nov23a_robot_web_controlled.ino
@@ -2,18 +2,22 @@
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+#include <WiFiUdp.h>              //Used for direct UDP control
 
-//Distance between wheels in cm
-#define WHEEL_DIST 8
-
+//Pins for DRV8833 motor driver
 enum pins
 {
-  LED = 16,
   LEFT_FORWARD = 5,
   LEFT_BACKWARD = 4,
   RIGHT_FORWARD = 0,
   RIGHT_BACKWARD = 2
 };
+
+const int PORT = 8080;
+WiFiUDP net;
+const int BUFF_LEN = 100;
+signed char buff[BUFF_LEN];
+unsigned long last_packet_time;
 
 ESP8266WebServer server(80);   //Web server object. Will be listening in port 80 (default for HTTP)
 
@@ -42,31 +46,71 @@ void driveMotors(float x, float z){
 }
 
 void motorHandler(){
+  //Use server x and z to drive motors - http://esp8266_ip_address/?x=1.0&z=-1.0
   driveMotors(server.arg("x").toFloat(), server.arg("z").toFloat());
+  //Just echo back inputs and send a 200 OK
   server.send(200, "text/plain", server.arg("x") + " " + server.arg("z"));  
 }
 
 void setup()
 {
   Serial.begin(115200);
-
+  //Setup up pins for motor controller
   pinMode(LEFT_FORWARD, OUTPUT);
   pinMode(LEFT_BACKWARD, OUTPUT);
   pinMode(RIGHT_FORWARD, OUTPUT);
   pinMode(RIGHT_BACKWARD, OUTPUT);
-  
-  analogWrite(LEFT_FORWARD, 0);
-  analogWrite(LEFT_BACKWARD, 0);
-  analogWrite(RIGHT_FORWARD, 0);
-  analogWrite(RIGHT_BACKWARD, 0);  
-  
+  //Make sure motors are stopped
+  driveMotors(0,0);
+
+  //Setup wifi
   WiFiManager wifiManager;
   wifiManager.autoConnect();
 
+  //Listen for web api calls
   server.on("/", motorHandler);
   server.begin();
+
+  //Listen for UDP controll
+  net.begin(PORT);
+  last_packet_time = millis();
 }
 
 void loop() {
-  server.handleClient();    //Handling of incoming requests
+  //Listen on port 80 for web api
+  server.handleClient();
+  //List on port 8080 for UPD controll
+  handleUdp();
+}
+
+void handleUdp(){
+  if (net.parsePacket() >= 2) {
+      last_packet_time = millis();
+      net.read((unsigned char*)buff, BUFF_LEN);
+      const int left = buff[0] * 8;
+      const int right = buff[1] * 8;
+      Serial.printf("left: %i - right: %i\n", left, right);
+      //If requested motor speed is above ~20%, move at requested speed and direction, otherwise stop.
+      if (left > 200) {
+        analogWrite(LEFT_FORWARD, left < 1023 ? left : 1023);
+        analogWrite(LEFT_BACKWARD, 0);
+      } else if (left < -200) {
+        analogWrite(LEFT_FORWARD, 0);
+        analogWrite(LEFT_BACKWARD, left > -1023 ? -left : 1023);
+      } else {
+        analogWrite(LEFT_FORWARD, 0);
+        analogWrite(LEFT_BACKWARD, 0);
+      }
+      if (right > 200) {
+        analogWrite(RIGHT_FORWARD, right < 1023 ? right : 1023);
+        analogWrite(RIGHT_BACKWARD, 0);
+      } else if (right < -200) {
+        analogWrite(RIGHT_FORWARD, 0);
+        analogWrite(RIGHT_BACKWARD, right > -1023 ? -right : 1023);
+      } else {
+        analogWrite(RIGHT_FORWARD, 0);
+        analogWrite(RIGHT_BACKWARD, 0);
+      }
+    }
+  
 }
